56d081ba34ce5362b895ce501fc6fa598836d8e2
VarelaCristianFacundo <cfacundovarela@yahoo.com>
2016-11-20 10:01:06 -0300

    Se agrega funcionalidad a apuestas de desafios

--- TPFinal/www/js/CtrlDesafio.js
+++ TPFinal/www/js/CtrlDesafio.js
@@ -3,6 +3,19 @@ angular.module('desafio.controller', [])
 .controller('desafioCtrl', function($scope) {
   $scope.nueva={};
 $scope.nueva.credito = 0;
+$scope.nueva.desc= "";
 $scope.cantPartidas;
+
+$scope.Apostar = function(){
+
+	alert("llegue");
+  firebase.database().ref("/desafios/").push({
+  desafiante: "Cristian",
+  Descripcion: $scope.nueva.desc,
+  Credito: $scope.nueva.credito
+  })
+ $scope.cantPartidas = $scope.cantPartidas + 1;
+
+};
 
 })
