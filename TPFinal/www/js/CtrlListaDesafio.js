54c305397d2e518a4fcae055a4216056c8be975f
VarelaCristianFacundo <cfacundovarela@yahoo.com>
2016-11-21 12:51:40 -0300

    Se recuperan datos de firebase para listadesafios

--- TPFinal/www/js/CtrlListaDesafio.js
+++ TPFinal/www/js/CtrlListaDesafio.js
@@ -2,21 +2,42 @@ angular.module('listadesafios.controller', [])
 
 .controller('listaDesafiosCtrl', function($scope, $timeout) {
 
-	$scope.desafios =[];
+
 	$scope.desafio = {};
 	$scope.desafio.desc = "";
 	$scope.desafio.cred = "";
+  $scope.desafios =[];
 
-	var refDesafios = new Firebase("https://loginsupervisada.firebaseio.com/desafios");
+  $scope.batalla = {};
+  $scope.batalla.desc = "";
+  $scope.batalla.cred = "";
+  $scope.batallas =[];
+
+
+	var refDesafios = new firebase.database().ref('desafios/');
 	console.info(refDesafios);
-	
     refDesafios.on('child_added', function(data){
+
     $timeout(function(){
-      console.info(data.val(), data.key());
+      console.info(data.val(), data.key);
       var desafio = data.val();
-      desafio.key = data.key();
+      desafio.key = data.key;
       $scope.desafios.push(desafio);
       console.info($scope.desafios);
+    });
+});
+
+
+    var refBatalla = new firebase.database().ref('partidasBatallaNaval/');
+    console.info(refBatalla);
+    refBatalla.on('child_added', function(data){
+
+    $timeout(function(){
+      console.info(data.val(), data.key);
+      var batalla = data.val();
+      batalla.key = data.key;
+      $scope.batallas.push(batalla);
+      console.info($scope.batallas);
     });
 });
 
