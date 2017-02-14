9e3bb882a8c1919ff735d99da0703e7df67f2bf9
VarelaCristianFacundo <cfacundovarela@yahoo.com>
2017-02-14 05:35:56 -0300

    Cargo credito leyendo el QR

--- TPFinal/www/js/CtrlPerfil.js
+++ TPFinal/www/js/CtrlPerfil.js
@@ -1,14 +1,31 @@
 angular.module('perfiluser.controller', [])
 
-.controller('perfiluserCtrl', function($scope, $cordovaBarcodeScanner, sLogueado) {
+.controller('perfiluserCtrl', function($scope, $timeout, $cordovaBarcodeScanner, sLogueado) {
 $scope.referencia = sLogueado.traerUser();
-
+$scope.tarjetas= [];
 
   $scope.LeerCodigo = function(){
-    console.info("dfsdf");
     $cordovaBarcodeScanner.scan().then ( function (imagenEscaneada){
       alert (imagenEscaneada.text);
       console.info("imagenEscaneada",imagenEscaneada);
+      var refTarjetas = new firebase.database().ref('tarjetas/'+ imagenEscaneada.text);
+
+      refTarjetas.on('child_added', function(data){
+
+      $timeout(function(){
+      if (data.key == "cantidad") {
+        $scope.referencia.creditos += data.val();
+      };
+      //console.info(data.val(), data.key);
+      /*var tarjeta = data.val();
+      tarjeta.key = data.key;
+      $scope.tarjetas.push(tarjeta);
+      console.info($scope.tarjetas);
+      $scope.referencia.creditos += $scope.tarjetas[0];*/
+    });
+
+});
+
     }, function(error){
       console.info("error",error);
       alert ("Ha ocurrido un error: " + error);
