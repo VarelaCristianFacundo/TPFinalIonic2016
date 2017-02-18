325595b94c6f13bc22ff57effba3ee526fb2a05b
VarelaCristianFacundo <cfacundovarela@yahoo.com>
2017-02-17 22:29:16 -0300

    Modificaciones de estilos sobre CSS

--- TPFinal/www/js/CtrlCargarCredito.js
+++ TPFinal/www/js/CtrlCargarCredito.js
@@ -2,7 +2,7 @@ angular.module('cargarcredito.controller', [])
 
 .controller('cargarcreditoCtrl', function($scope) {
 $scope.nueva={};
-$scope.nueva.credito = 0;
+$scope.nueva.credito;
 
 $scope.CargarCredito = function(){
 
